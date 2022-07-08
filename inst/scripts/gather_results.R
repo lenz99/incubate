aa243a4189e175eaf2dac5a0d86df2da625f18ba
Matthias Kuhn <matthias.kuhn@tu-dresden.de>
2022-07-08 15:36:22 +0200

    gather_results script: make it work for confint-results as well. version bump v1.1.9.9011

--- inst/scripts/gather_results.R
+++ inst/scripts/gather_results.R
@@ -2,6 +2,9 @@
 # mkuhn, 2022-07-07
 # gather individual simulation results
 
+
+# setup --------------------------------------------------------------------
+
 library('rlang')
 suppressPackageStartupMessages(library('purrr'))
 suppressPackageStartupMessages(library('dplyr'))
@@ -23,8 +26,8 @@ if (any(c('help', 'h') %in% names(cmdArgs))){
   cat('Parameter options are:\n')
   cat('  --help\t print this help\n')
   cat('  --resultsDir=\t specify the directory where to find and to put the result files. Defaults to directory "results".\n')
-  cat('  --resultsName=\t specify a name suffix for results file. Default is "MS".\n')
-  cat('  --type=\t specify if we gather results for "test" (default) or "confint".\n')
+  cat('  --resultsName=\tspecify a name suffix for results file. Default is "MS".\n')
+  cat('  --type=\t what type of results to gather? "test" (default) or "confint"\n')
   cat('  --removeTemp\t flag to clean temporary results file after they have been saved.\n')
   quit(save = 'no')
 }
@@ -56,21 +59,34 @@ RUN_NS <- tidyr::crossing(L1=LETTERS, L2=LETTERS) %>%
   dplyr::transmute(L=paste0(L1, L2)) %>%
   dplyr::pull(L)
 
+
+# previous results --------------------------------------------------------
+
 # look at previous results (already saved)
 resData <- NULL
 indOffset <- 0L
 RES_FILEN <- file.path(myResultsDir, paste0('simRes_', myType, '_', myResultsName,'.rds'))
+
 if (file.exists(RES_FILEN)){
 	resData <- readRDS(RES_FILEN)
+
 	if ( ! is.list(resData) || is.null(names(resData))){
-		cat('\nResults file is not a list! Start over from scratch!\n')
+		cat('\nResults file is not a list! We start over from scratch now!\n')
 		resData <- NULL
 	} else {
-		cat(glue('There are already {length(resData)} entries saved!'), '\n')
-		indOffset <- max(0L, which(RUN_NS %in% purrr::map_chr(resData, ~ { substr(.x[['run']][[1L]], start = 1L, stop = 2L) })), na.rm = TRUE)
-		cat(glue('Offset index is {indOffset}.'), '\n')
+	  cat(glue('There have been {length(resData)} entries saved already!'), '\n')
+	  # run numbers only for tests
+	  if (myType == 'test'){
+	    indOffset <- max(0L,
+	                     which(RUN_NS %in% purrr::map_chr(resData, ~ { substr(.x[['run']][[1L]], start = 1L, stop = nchar(RUN_NS[[1L]])) })),
+	                     na.rm = TRUE)
+	    cat(glue('Offset index is {indOffset}.'), '\n')
+	  }
 	}
 }
+
+
+# read in temporary results data --------------------------------------------------
 
 # read in temporary result files, process the results
 # @return list name (from metadata) and data (unnested)
@@ -81,12 +97,16 @@ readResultFile <- function(rdsFN, ind) {
 	stopifnot( is.list(mdList), all( c('host', 'time') %in% names(mdList)) )
 
 	resName <- paste(mdList[['host']], mdList[['time']], sep='||')
-	stopifnot( indOffset + ind <= length(RUN_NS) )
 
 	unnestVar <- c('P', 'ci_res')[[1L+(myType == 'confint')]]
 	resData <- rdsF %>%
-		tidyr::unnest(cols = all_of(unnestVar)) %>%
-		dplyr::mutate(run = paste0(RUN_NS[[indOffset + ind]], run))
+		tidyr::unnest(cols = all_of(unnestVar))
+
+	if (myType == 'test'){
+	  stopifnot( indOffset + ind <= length(RUN_NS) )
+	  resData <- resData %>%
+	    dplyr::mutate(run = paste0(RUN_NS[[indOffset + ind]], run))
+	}
 
 	# pass on comment to unnested dataframe
 	comment(resData) <- rdsFC
@@ -94,9 +114,15 @@ readResultFile <- function(rdsFN, ind) {
 	rlang::list2(!!resName := resData)
 }
 
+
 resCandidates <- purrr::imap(.x = simResFileNames, .f = readResultFile) %>%
   # drop list level from map
   purrr::flatten()
+
+
+
+# save all results data ---------------------------------------------------
+
 
 if (is.null(resData)){
 	cat('\nStart with fresh results from scratch!\n')
@@ -104,8 +130,8 @@ if (is.null(resData)){
 } else {
 	resDuplicates <- intersect(names(resData), names(resCandidates))
 	if ( length(resDuplicates) ){
-		cat(glue("Temporary results are already stored in results list:",
-		         "{paste(resDuplicates, collapse = '\n  * ')}"), "\n\n")
+		cat(glue("These temporary result dataframes are already stored in the {myResultsName}-results file:\n  * ",
+		         "{paste(resDuplicates, collapse = '\n  * ')}", .trim = FALSE), "\n\n")
 		cat('Please clean up temporary results files that are already saved in result list, first!\n')
 		q(save='no')
 	}
