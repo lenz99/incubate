22a79541319235bd22c7a3ed1ce63c4d02d7fc59
Matthias Kuhn <matthias.kuhn@tu-dresden.de>
2023-08-29 12:01:51 +0200

    simul_test.R: clean up. more flexible when slicing from scenarios for simulation dev version bump

--- inst/scripts/simul_test.R
+++ inst/scripts/simul_test.R
@@ -6,6 +6,7 @@
 
 # init -----
 
+cat("MC-simulations for test for difference in delay parameters.\n")
 cat("It is ***", toString(Sys.time()), "***\n")
 
 library("incubate")
@@ -51,7 +52,7 @@ if (any(c('help', 'h') %in% names(cmdArgs))){
   cat('  --scenario=\t with respect to the delay in both groups, choose a scenario for the simulation:\n\t\t\tDELAYEQ = no difference in delay,\n\t\t\tDELAYGT = 2nd group y with bigger delay.\n\t\t\tMS = only relevant scenarios shown in manuscript (default)\n\t\t\tALL = all cases\n')
   cat('  --allN\t use different sample sizes in the simulations. Without this option, only a single sample size is used.\n')
   cat('  --scaleSimple\t use only standard value for scale and scale-ratio\n')
-  cat('  --slice=\t if given, pick only this number of scenarios for the simulation\n')
+  cat('  --slice=\t if given, pick only this number of first scenarios for simulations. If negative, scenarios taken from the tail.\n')
   cat('  --seed=\t if given, set random seed at the start of the script. Default is date-dependent.\n')
   cat('  --chnkSize=\t chunk size to write out results having processed so many scenarios. Default is no chunking (=0).\n')
   cat('  --workers=\t number of parallel computations using `future.callr` and `future.apply`. The only level of parallelization is across the MC-replications for each simulation setting.\n')
@@ -81,10 +82,11 @@ myR <- cmdArgs[["R"]]
 stopifnot( is.numeric(myR), length(myR) == 1L, myR >= 1L )
 
 myMCNrep <- cmdArgs[["mcnrep"]]
-stopifnot( is.numeric(myMCNrep), length(myMCNrep) == 1L, myMCNrep >= 1L )
+stopifnot(is.numeric(myMCNrep), length(myMCNrep) == 1L, myMCNrep >= 1L)
 
 mySlice <- cmdArgs[["slice"]]
-stopifnot( ! is.null(mySlice), is.numeric(mySlice), length(mySlice) == 1L )
+stopifnot(! is.null(mySlice), is.numeric(mySlice), length(mySlice) == 1L)
+mySlice <- ceiling(mySlice)
 
 mySeed <- cmdArgs[["seed"]]
 stopifnot( is.numeric(mySeed), length(mySeed) == 1L, mySeed >= 0L )
@@ -103,20 +105,19 @@ myScaleSimple <- isTRUE(any(c("scalesimple", "scale", "scales") %in% tolower(nam
 
 if (mySeed > 0L) set.seed(mySeed)
 
-# shape values according to distribution
-
 simSetting <- tidyr::expand_grid(n_x = c(8, 10, 12, 15, 20, 30, 50, 100),
                                  delay_x = 5,
                                  delay_y = c(5, 7, 9, 11, 13, 15), #, 20, 100, 1000),
                                  scale_x = c(5, 10), #c(1, 2, 5),
                                  scale_ratio = c(2, 1, .5),
-                                 # effectively filter for distribution
+                                 # shape values according to distribution
+                                 #+effectively filter for distribution
                                  shape = if (isExpon) 1 else c(.5, 2))
 
 # avoid duplicates:
 # by convention, group y is not less delayed than group x
 simSetting <- simSetting %>%
-  dplyr::filter(delay_y >= delay_x - 1e-11) %>%
+  dplyr::filter(delay_y >= delay_x) %>%
   # use equally sized groups
   dplyr::mutate(n_y = n_x, .after = n_x)
 
@@ -124,63 +125,78 @@ simSetting <- simSetting %>%
 # default is to use only the smallest sample size
 if (!myAllN) {
   simSetting <- simSetting %>%
-    dplyr::filter(n_x == min(n_x))
+    dplyr::slice_min(n_x)
 }
 
 if (myScaleSimple) {
   simSetting <- simSetting %>%
-    dplyr::filter(dplyr::near(scale_x, 10), dplyr::near(scale_ratio, 1))
+    dplyr::filter(dplyr::near(scale_x, 10),
+                  dplyr::near(scale_ratio, 1))
 }
 
-#filter only relevant scale_ratio combinations
-# for exponential:
-# [G1] scale_x = 10 & scale_ratio = 1  (rate_x = .1, rate_ratio = 1)
-# [G2] scale_x =  5 & scale_ratio = 1  (rate_x = .2, rate_ratio = 1)
-# [G3] scale_x =  5 & scale_ratio =  2 [i.e., scale_y = 10]  (rate_x = .2, rate_ratio = .5 [i.e., rate_y = .1])
-# [G4] scale_x = 10 & scale_ratio = .5 [i.e., scale_y =  5]  (rate_x = .1, rate_ratio = 2  [i.e., rate_y = .2])
-# for weibull:
-# dd=0, k=.5|2, scale_x = 10 & scale_ratio = 1
-# dd=5, k=.5|2, scale_x = 10 & scale_ratio = 1
-
-# filter based on scale parameters
-# this contains the cases which are needed in the manuscript
-simFilterMS <- if (isExpon) {
-  simSetting %>%
-    dplyr::filter(dplyr::near(scale_ratio, 1) | dplyr::near(scale_x, 5) & dplyr::near(scale_ratio, 2) |
-                    dplyr::near(scale_x, 10) & dplyr::near(scale_ratio, .5)) } else
-      tibble::tibble(scale_x = 10, scale_ratio = 1)
 
 
 # filter for target scenario!
 # use all capital letters (see definition of myScenario)
-switch (myScenario,
-        DELAYEQ = {
-          simSetting <- simSetting %>%
-            dplyr::filter(dplyr::near(delay_x, delay_y))
-                          # # for both distributions:
-                          # dplyr::near(scale_x, 10), dplyr::near(scale_ratio, 1))
-        },
+simSetting <- switch (myScenario,
+                      DELAYEQ = {
+                        simSetting %>%
+                          dplyr::filter(dplyr::near(delay_x, delay_y))
+                        # # for both distributions:
+                        # dplyr::near(scale_x, 10), dplyr::near(scale_ratio, 1))
+                      },
 
-        DELAYGT = {
-          simSetting <- simSetting %>%
-            dplyr::filter(delay_y > delay_x + 1e-11)
-            #dplyr::inner_join(simFilter, by = names(simFilter))
-        },
+                      DELAYGT = {
+                        simSetting %>%
+                          dplyr::filter(delay_y > delay_x + 1e-11)
+                      },
 
-        MS = {
-          simSetting <- simSetting %>%
-            dplyr::inner_join(simFilterMS, by = colnames(simFilterMS))
-        },
+                      MS = {
 
-        ALL = { invisible(NULL) }, # no-op, keep everything! (also unused border cases)
-        stop("Unknown target!")
+                        #filter only relevant scale_ratio combinations
+                        # for exponential:
+                        # [G1] scale_x = 10 & scale_ratio = 1  (rate_x = .1, rate_ratio = 1)
+                        # [G2] scale_x =  5 & scale_ratio = 1  (rate_x = .2, rate_ratio = 1)
+                        # [G3] scale_x =  5 & scale_ratio =  2 [i.e., scale_y = 10]  (rate_x = .2, rate_ratio = .5 [i.e., rate_y = .1])
+                        # [G4] scale_x = 10 & scale_ratio = .5 [i.e., scale_y =  5]  (rate_x = .1, rate_ratio = 2  [i.e., rate_y = .2])
+                        # for weibull:
+                        # dd=0, k=.5|2, scale_x = 10 & scale_ratio = 1
+                        # dd=5, k=.5|2, scale_x = 10 & scale_ratio = 1
+
+                        # filter based on scale parameters
+                        # this contains the cases which are needed in the manuscript
+                        local({
+                          simFilterMS <- if (isExpon) {
+                            simSetting %>%
+                              dplyr::filter(dplyr::near(scale_ratio, 1) |
+                                              (dplyr::near(scale_x, 5) & dplyr::near(scale_ratio, 2)) |
+                                              (dplyr::near(scale_x, 10) & dplyr::near(scale_ratio, .5)))
+                          } else {
+                            tibble::tibble(scale_x = 10, scale_ratio = 1)
+                          }
+
+                          simSetting %>%
+                            dplyr::inner_join(simFilterMS, by = colnames(simFilterMS))
+                        })
+                      },
+
+                      # ALL = no-op, keep everything! (also unused border cases)
+                      ALL = {
+                        simSetting
+                      },
+                      stop("Unknown target!", call. = FALSE)
 )
 
 
 
-if (mySlice > 0L) {
-  simSetting <- simSetting %>%
-    dplyr::slice(seq_len(length.out = ceiling(mySlice)))
+if (! dplyr::near(mySlice, 0)) {
+  simSetting <- local({
+
+    sliceF <- if (mySlice > 0) dplyr::slice_head else dplyr::slice_tail
+
+    simSetting %>%
+      sliceF(n = abs(mySlice))
+  })
 }
 
 if (myPrint) {
@@ -191,6 +207,7 @@ if (myPrint) {
   cat('A bootstrap test has R=', myR, 'parametric bootstrap samples (P-value resolution).\n')
   cat('Seed set initially is: ', if (mySeed>0) mySeed else '-not set-', '\n')
   cat('Results directory is set to ', myResultsDir, '\n')
+  cat("~fine~\n")
   quit(save = 'no')
 }
 
@@ -208,79 +225,8 @@ if (myWorkers > 1L) {
 }
 
 
+
 # functions -----
-
-
-#' Monte-Carlo simulation for a single simulation setting.
-#' Uses parallel computation (future_replicate) to go through the (=nrep) MC-simulations.
-#' Each bootstrap test is also future-aware (and would pick up a nested future-plan setting)
-#' @param xx numeric. parameters that specify the simulation model for both groups
-#' @return dataframe. P-values in the different Monte-Carlo runs.
-doMCSim_OLD <- function(xx) {
-  n_x <- xx[[1]]
-  n_y <- xx[[2]]
-  delay_x <- xx[[3]]
-  delay_y <- xx[[4]]
-
-  scale_x <- xx[[5]]
-  scale_ratio <- xx[[6]]
-  shape <- xx[[7]]
-
-
-  testList <- future.apply::future_replicate(n = myMCNrep, expr = {
-    # generate data
-    x <- y <- 1 #dummy init
-    if (isExpon) {
-      stopifnot( dplyr::near(shape, 1L) )
-      x <- rexp_delayed(n = n_x, delay = delay_x, rate = 1/scale_x)
-      y <- rexp_delayed(n = n_y, delay = delay_y, rate = 1/(scale_x * scale_ratio))
-    } else {
-      # weibull
-      x <- rweib_delayed(n = n_x, delay = delay_x, scale = scale_x, shape = shape)
-      y <- rweib_delayed(n = n_y, delay = delay_y, scale = scale_x * scale_ratio, shape = shape)
-    }
-
-    te_diff <- NULL
-    # test_diff might also use parallel computations depending on future-settings
-    try(expr = {
-      te_diff <- test_diff(x = x, y = y, distribution = 'expon', param = 'delay', method = "MPSE", R = myR, type = "all")
-      # get bootstrap P-value for combined test: delay+rate (we request it only if the scale (=1/rate for exponential) is indeed different)
-      if (scale_ratio != 1){
-        te_diff2 <- test_diff(x = x, y = y, distribution = 'expon', param = c('delay', 'rate'), method = "MPSE", R = myR, type = 'bootstrap')
-        # store P-value of delay+rate in original test_diff-object
-        te_diff$P$bootstrap2 <- purrr::pluck(te_diff2, 'P', 'bootstrap', .default = NA_real_)
-      }#fi
-    }, silent = TRUE)
-    te_diff
-  }, simplify = FALSE, future.seed = TRUE)
-
-  # drop NULLs
-  testList <- purrr::compact(testList)
-
-  # long dataframe of P-values
-  tibble::tibble(
-    run = seq_along(testList),
-    R_nominal = purrr::map_dbl(testList, "R"),
-    R_effective = purrr::map_dbl(testList, list("testDist", length), .default = NA_real_),
-    # likelihood-ratio style test
-    lr = purrr::map_dbl(testList, list("P", "LR"), .default = NA_real_),
-    boot  = purrr::map_dbl(testList, list("P", "bootstrap"), .default = NA_real_),
-    boot2 = purrr::map_dbl(testList, list("P", "bootstrap2"), .default = NA_real_),
-    gof_moran0 = purrr::map_dbl(testList, list("P", "moran"), .default = NA_real_),
-    gof_moran1 = purrr::map_dbl(testList, list("P", "moran1"), .default = NA_real_),
-    gof_pearson0 = purrr::map_dbl(testList, list("P", "pearson"), .default = NA_real_),
-    gof_pearson1 = purrr::map_dbl(testList, list("P", "pearson1"), .default = NA_real_),
-    # AD-test is currently not recommended
-    #gof_ad0 = purrr::map_dbl(testList, list("P", "ad"), .default = NA_real_),
-    #gof_ad1 = purrr::map_dbl(testList, list("P", "ad1"), .default = NA_real_),
-    logrank = purrr::map_dbl(testList, list("P", "logrank"), .default = NA_real_),
-    logrank_pp = purrr::map_dbl(testList, list("P", "logrank_pp"), .default = NA_real_) ) %>%
-    # make long format, all output (mostly P-values) into column 'value'
-    tidyr::pivot_longer(cols = !run, names_to = "method", values_to = "value",
-                        # drop NAs (for instance, missing boot2 P-value)
-                        values_drop_na = TRUE)
-
-}
 
 #' Run Monte-Carlo simulations to test difference in delay using an exponential model for a given simulation setting
 #'
@@ -291,7 +237,7 @@ doMCSim_OLD <- function(xx) {
 #' @return dataframe. P-values in the different Monte-Carlo runs.
 doMCSim <- function(DGPsetting) {
   # settings from the environment:
-  stopifnot( exists("isExpon"), exists("myMCNrep"), exists("myR") )
+  stopifnot(exists("isExpon"), exists("myMCNrep"), exists("myR"))
 
   n_x <- DGPsetting[[1]]
   n_y <- DGPsetting[[2]]
@@ -334,9 +280,12 @@ doMCSim <- function(DGPsetting) {
                                                        te_diff <- NULL
                                                        # test_diff might also use parallel computations depending on future-settings
                                                        try(expr = {
+                                                         # test difference in delay1 in exponential model
                                                          te_diff <- test_diff(x = x, y = y, distribution = "expon", param = "delay1",
-                                                                              method = method, profiled = profiled, R = R, type = "all", doLogrank = method == "MPSE" && !profiled)
-                                                         # get bootstrap P-value for combined test: delay+rate only if the scale (=1/rate for exponential) is indeed different betw groups
+                                                                              method = method, profiled = profiled, R = R, type = "all",
+                                                                              # log-rank test only once
+                                                                              doLogrank = method == "MPSE" && !profiled)
+                                                         # bootstrap P-value for combined test: delay+rate only if the scale (=1/rate for exponential) is indeed different betw groups
                                                          if (testParamCombined) {
                                                            te_diff2 <- test_diff(x = x, y = y, distribution = "expon", param = c("delay1", "rate1"),
                                                                                  method = method, profiled = profiled, R = R, type = "bootstrap")
@@ -348,8 +297,8 @@ doMCSim <- function(DGPsetting) {
                                                      })) %>%
                                                      # compact testDiff-list column: drop entries that did not work out!
                                                      dplyr::filter(! is.null(testDiffObj)) %>%
-                                                     # extract all P-values/R_eff in long format from each row!
-                                                     #+dplyr::reframe (beta v1.1.0) allows to summarize with more than one row
+                                                     # extract all P-values/R_eff in long format from each row in estimMethods-df!
+                                                     #+dplyr::reframe (beta in v1.1.0) allows to summarize with more than one row
                                                      dplyr::reframe(method, profiled, R,
                                                                     R_eff = length(testDiffObj$testDist),
                                                                     tibble::enframe(unlist(testDiffObj$P),
@@ -365,15 +314,21 @@ doMCSim <- function(DGPsetting) {
 
 
 #' Run MC-simulations for each scenario sequentially (row-by-row)
-#' @param simSetDF
-#' @returns tibble of simulations settings where results tibble has been added
+#' @param simSetDF dataframe containing simulation scenarios
+#' @returns tibble of simulations settings with results added
 applyMCSims <- function(simSetDF) {
   simSetDF %>%
     dplyr::mutate(., results = apply(as.matrix(.), MARGIN = 1L, FUN = doMCSim))
 }
 
 
-# run & save ----
+
+#' Add meta data to dataframe
+#'
+#' The special `comment` attribute is used to store the deparsed meta data list.
+#' @param da simulation data
+#' @param timeTag time stamp to be added in meta data comment
+#' @returns simulation data with meta data added as comment
 addMetaData <- function(da, timeTag) {
   # add comment as text
   comment(da) <- list(seed = mySeed, R = myR, mcnrep = myMCNrep, workers = myWorkers, chnkSize = myChnkSize,
@@ -384,8 +339,13 @@ addMetaData <- function(da, timeTag) {
                       time = timeTag) %>%
     #paste(names(.), ., sep = '=', collapse = ',')
     deparse()
+
   da
 }
+
+
+
+# run & save ----
 
 DATETIME_TAG <- format(Sys.time(), format = "%Y-%m-%d-%Hh%Mm%Ss")
 rdsBaseName <- paste0("simRes_test_", DATETIME_TAG)
@@ -395,41 +355,50 @@ if (myChnkSize < 1L || NROW(simSetting) <= myChnkSize) {
   # no chunking
   simSetting <- applyMCSims(simSetDF = simSetting) %>%
     addMetaData(timeTag = DATETIME_TAG)
+
   saveRDS(simSetting, file = rdsName)
+
 } else {
+
   # work in chunks
   rowIdx <- seq_len(NROW(simSetting))
+  # how many chunks?
   chnkNbr <- (length(rowIdx) %/% myChnkSize)+1L
-  stopifnot( chnkNbr > 1L, chnkNbr <= 999999L )
+  stopifnot(chnkNbr > 1L, chnkNbr <= 999999L)
   # stripe over the scenarios
   rowIdxLst <- split(rowIdx, f = rep_len(x=seq_len(chnkNbr), length.out = length(rowIdx)))
-  stopifnot( length(rowIdxLst) == chnkNbr )
-  for (i in seq_along(rowIdxLst)){
-    simSetting_chnk <- dplyr::slice(simSetting, rowIdxLst[[i]])
-    simSetting_chnk <- applyMCSims(simSetDF = simSetting_chnk)
-    cat("Write out chunk ", i, "\n")
-    saveRDS(simSetting_chnk %>% addMetaData(timeTag = DATETIME_TAG),
+  stopifnot(length(rowIdxLst) == chnkNbr)
+
+  for (i in seq_along(rowIdxLst)) {
+    simSetting_chnk <- dplyr::slice(simSetting, rowIdxLst[[i]]) %>%
+      applyMCSims() %>%
+      addMetaData(timeTag = DATETIME_TAG)
+    #simSetting_chnk <- applyMCSims(simSetDF = simSetting_chnk)
+
+    message("Writing out chunk ", i, " to RDS-file..")
+    saveRDS(simSetting_chnk,
             file = file.path(myResultsDir, paste0(rdsBaseName, "_", sprintf("%06d", i), ".rds")))
   }#rof
 
   # merge chunked output!
-  chnkFileNames <- list.files(path = myResultsDir, pattern = paste0('^', rdsBaseName, '_[[:digit:]]+[.]rds$'),
-             full.names = TRUE)
+  chnkFileNames <- list.files(path = myResultsDir,
+                              pattern = paste0('^', rdsBaseName, '_[[:digit:]]+[.]rds$'),
+                              full.names = TRUE)
   if (length(chnkFileNames)) {
-    chnkFiles <- purrr::map(.x = chnkFileNames, .f = readRDS)
+    # re-create complete simSetting data
+    simSetting <- purrr::map(.x = chnkFileNames, .f = readRDS) %>%
+      dplyr::bind_rows() %>%
+      addMetaData(timeTag = DATETIME_TAG)
 
-    saveRDS(chnkFiles %>%
-              dplyr::bind_rows() %>%
-              addMetaData(timeTag = DATETIME_TAG),
-            file = rdsName)
+    saveRDS(simSetting, file = rdsName)
 
-    if ( file.exists(rdsName) && (! exists('infoRDS') || ! inherits( try(infoRDS(rdsName), silent = TRUE), "try-error")) ){
+    if (file.exists(rdsName) && (! exists('infoRDS') || ! inherits( try(infoRDS(rdsName), silent = TRUE), "try-error"))) {
       message("Removing ", length(chnkFileNames), " intermediate chunked RDS-files!")
       try(file.remove(chnkFileNames))
     } #fi remove RDS-chunk-files
 
   } else {
-    warning("Did not find chunked RDS-output.")
+    warning("Did not find chunked RDS-output.", call. = FALSE)
   }
 
 } #esle chunking
