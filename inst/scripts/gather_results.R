b21c96c954788ccee3f53f2b78210fd2dfb991bf
Matthias Kuhn <matthias.kuhn@tu-dresden.de>
2023-09-07 11:45:48 +0200

    gather_results: update superseded code: transmute (dplyr) flatten (purrr)
    better doc

--- inst/scripts/gather_results.R
+++ inst/scripts/gather_results.R
@@ -1,38 +1,39 @@
 #!/usr/bin/env Rscript
 # mkuhn, 2022-07-07
-# gather individual simulation results
+# gather individual simulation results from temporary files
 
 
 # setup --------------------------------------------------------------------
 
-library('incubate')
-cat('incubate package version: ', toString(packageVersion('incubate')), '\n')
+library("incubate")
+cat("incubate package version: ", toString(packageVersion("incubate")), "\n")
 
 library('rlang')
 library('dplyr', warn.conflicts = FALSE)
 library('tidyr', warn.conflicts = FALSE)
-library('purrr')
+library("purrr", warn.conflicts = FALSE)
 library('glue')
 suppressPackageStartupMessages(library('R.utils'))
-
+stopifnot(packageVersion("purrr") > "1.0.0") #for list_flatten()
 
 
 cmdArgs <- R.utils::commandArgs(trailingOnly=TRUE,
                                 asValues = TRUE,
                                 excludeReserved = FALSE, excludeEnvVars = TRUE,
-                                defaults = list(resultsDir=file.path(getwd(), 'results'),
-                                                resultsTag='MS',
-                                                type = 'test'))
+                                defaults = list(resultsDir=file.path(getwd(), "results"),
+                                                resultsTag="MS",
+                                                type = "test"))
 
-if (any(c('help', 'h') %in% names(cmdArgs))){
-  cat('Gather Monte-Carlo simulation results and save it as a common list.\n')
+if (any(c('help', 'h') %in% names(cmdArgs))) {
+  cat('Gather Monte-Carlo simulation results from temporary RDS result files and save it as a common list.\n')
   cat('Temporary data files have a date tag in their name.\n')
   cat('Parameter options are:\n')
   cat('  --help\t print this help\n')
-  cat('  --resultsDir=\t specify the directory where to find and to put the result files. Defaults to sub-directory "results" of current directory.\n')
+  cat('  --resultsDir=\t specify the directory where to find and also where to put the result files. Defaults to sub-directory "results" of working directory.\n')
   cat('  --resultsTag=\t specify a name suffix for results file. Default is "MS".\n')
   cat('  --type=\t what type of results to gather? "test" (default) or "confint"\n')
   cat('  --removeTemp\t flag to clean temporary results file after they have been saved.\n')
+
   quit(save = 'no')
 }
 
@@ -54,14 +55,15 @@ simResFileNames <- list.files(myResultsDir,
                               full.names=TRUE)
 
 if (! length(simResFileNames)) {
-	cat('No temporary result files found!\n')
-	q(save='no')
+	cat("No matching temporary result files for ", myType,
+	    " were found in sub-directory ", myResultsDir, "!\n")
+	q(save="no")
 }
 
 
-# run-namespace
+# create namespace for the different runs as vector
 RUN_NS <- tidyr::crossing(L1=LETTERS, L2=LETTERS) %>%
-  dplyr::transmute(L=paste0(L1, L2)) %>%
+  dplyr::mutate(L=paste0(L1, L2), .keep = "none") %>%
   dplyr::pull(L)
 
 
@@ -73,23 +75,23 @@ indOffset <- 0L
 RES_FILEN <- file.path(myResultsDir, paste0('simRes_', myType, '_', myResultsTag,'.rds'))
 
 # prepare index offset (if there are previous results)
-if (file.exists(RES_FILEN)){
+if (file.exists(RES_FILEN)) {
 	resData <- readRDS(RES_FILEN)
 
-	if ( ! is.list(resData) || is.null(names(resData))){
+	if ( ! is.list(resData) || is.null(names(resData))) {
 	  # no results data!
-		cat('\nResults file is not a list! We start over from scratch now!\n')
+		cat('\nResults file is not a list! We start over from scratch!\n')
 		resData <- NULL
 	} else {
 	  # results data found!
-	  cat(glue('There have been {length(resData)} entries saved already!'), '\n')
+	  cat(glue("There have been {length(resData)} entries saved already!"), "\n")
 	  # run-numbers only for tests (currently not for confint)
-	  if (myType == 'test'){
+	  if (myType == "test") {
 	    indOffset <- max(0L,
 	                     which(RUN_NS %in% purrr::map_chr(resData,
 	                                                      .f = ~ { substr(.x[['run']][[1L]], start = 1L, stop = nchar(RUN_NS[[1L]])) })),
 	                     na.rm = TRUE)
-	    cat(glue('Offset index is {indOffset}.'), '\n')
+	    cat(glue("Offset index from saved results is {indOffset}."), "\n")
 	  }#fi
 	}#fi resData
 }
@@ -98,21 +100,24 @@ if (file.exists(RES_FILEN)){
 # read in temporary results data --------------------------------------------------
 
 # read in temporary result file, process the results
-# @return list name (from metadata) and data (unnested)
+# @param rdsFN file name of RDS-file containing results
+# @param ind index number of given rds filename
+# @return named list containing the unnested data
 readResultFile <- function(rdsFN, ind) {
 	rdsF <- readRDS(rdsFN)
 	rdsFC <- comment(rdsF)
 	mdList <- eval(parse(text = rdsFC))
-	stopifnot( is.list(mdList), all( c('host', 'time') %in% names(mdList)) )
+	stopifnot(is.list(mdList), all(c('host', 'time') %in% names(mdList)))
 
 	resName <- paste(mdList[['host']], mdList[['time']], sep='||')
 
-	unnestVar <- if (myType == 'confint') 'ci_res' else 'results'
+	unnestVar <- if (myType == "confint") "ci_res" else  "results"
+	stopifnot(all(unnestVar %in% names(rdsF)))
 	resData <- rdsF %>%
 		tidyr::unnest(cols = all_of(unnestVar))
 
-	if (myType == 'test'){
-	  stopifnot( indOffset + ind <= length(RUN_NS) )
+	if (myType == "test") {
+	  stopifnot(indOffset + ind <= length(RUN_NS))
 	  resData <- resData %>%
 	    # prepend run namespace, like 'AS'
 	    dplyr::mutate(run = paste0(RUN_NS[[indOffset + ind]], run))
@@ -127,32 +132,38 @@ readResultFile <- function(rdsFN, ind) {
 
 resCandidates <- purrr::imap(.x = simResFileNames, .f = readResultFile) %>%
   # drop imap's additional list level
-  purrr::flatten()
+  purrr::list_flatten()
 
 
 
 # save all results data ---------------------------------------------------
 
-
-if (is.null(resData)){
+if (is.null(resData)) {
 	cat('\nStart with fresh results from scratch!\n')
 	saveRDS(resCandidates, file = RES_FILEN)
 } else {
   # check for duplicates
 	resDuplicates <- intersect(names(resData), names(resCandidates))
-	if ( length(resDuplicates) ){
+	if (length(resDuplicates)) {
 		cat(glue("These temporary result dataframes are already stored in the {myResultsTag}-results file:\n  * ",
 		         "{paste(resDuplicates, collapse = '\n  * ')}", .trim = FALSE), "\n\n")
 		cat('Please clean up temporary results files that are already saved in result list, first!\n')
 		q(save='no')
 	}
-	cat('\nAdd result candidates to existing result list!\n')
+	cat("\nAdd result candidates to existing result list!\n")
 	saveRDS(c(resData, resCandidates), file = RES_FILEN)
 }
 
-if (myRemoveTemp){
-  cat('\nAbout to remove temporary result files!\n')
-  file.remove(simResFileNames)
+if (myRemoveTemp) {
+  cat("\n")
+  cat("About to remove temporary result files!\n")
+  fnRmv <- file.remove(simResFileNames)
+  cat("Successfully removed ", sum(fnRmv), " temporary RDS-files.\n")
+  if (sum(fnRmv)) {
+    cat("\n  * ")
+    cat(paste(simResFileNames[fnRmv], collapse = "\n  * "))
+  }
+  cat("\n")
 }
 
-cat('\n~fine~\n')
+cat("\n~fine~\n")
